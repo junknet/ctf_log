85c53758c94c1a21807a22083530df6d85a17576
junknet <beini87@outlook.com>
2020-08-19 23:47:28 +0800

    gif split

---
+++ misc/gif_to_png.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+import os
+
+gifFileName = './4b0799f9a4d649f09a882b6b1130bb70.gif'
+im = Image.open(gifFileName)
+
+
+while True:
+    current = im.tell()
+    im.save(str(current)+'.png')
+    im.seek(current+1)
